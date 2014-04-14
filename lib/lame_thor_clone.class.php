7b7c9b4cec58d0060680be86f6026747b13fbcd9
Harry Metcalfe <harry@dxw.com>
2014-04-14 17:53:18 +0100

    pre-start

---
+++ lib/lame_thor_clone.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class LameThorClone {
+  private $executable;
+  private $options;
+  private $commands;
+
+  public function start($argv) {
+
+  }
+
+  protected function command($)
+};
---
+++ lib/whippet.class.php
@@ -0,0 +1,9 @@
+<?php
+
+require "lame_thor_clone.class.php";
+
+class Whippet extends LameThorClone {
+  public function help() {
+
+  }
+};
