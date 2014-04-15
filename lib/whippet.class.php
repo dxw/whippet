c2f61974ab545dd06175d856c92417b07ba7cfcf
Harry Metcalfe <harry@dxw.com>
2014-04-15 12:51:43 +0100

    Removed optionparser and made LameThorClone into its own thing

--- lib/whippet.class.php
+++ lib/whippet.class.php
@@ -1,14 +1,20 @@
 <?php
 
-require "lame_thor_clone.class.php";
+require "lame_thor_clone/lame_thor_clone.class.php";
 
 class Whippet extends LameThorClone {
 
   public function commands() {
-    $this->command('hello NAME', 'say hello to NAME');
+    $this->command('hello NAME', 'say hello to NAME', function($option_parser) {
+      $option_parser->addHead("Says hello to a person.\n");
+      $option_parser->addRule('b|big', "Congratulates NAME on their bigness");
+    });
   }
 
   public function hello($name) {
     echo "Hi, $name!\n";
+    if(isset($this->options->big)) {
+      echo "Aren't you a big fella?\n";
+    }
   }
 };
