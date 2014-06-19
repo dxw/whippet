5eda04b295b84b52cdb2193a61e4cc58d308a3dc
Harry Metcalfe <harry@dxw.com>
2014-06-19 17:20:20 +0100

    Further migration fixes for cp -a interoperability

--- generators/migration/generate.php
+++ generators/migration/generate.php
@@ -186,12 +186,12 @@ class MigrationGenerator {
     echo "Copying project plugins\n";
     foreach($plugins as $plugin_file => $plugin_data) {
       if(dirname($plugin_file) != '.') {
-        system("cp -a '{$old}/plugins/" . dirname($plugin_file) . "/.' '{$new}/wp-content/plugins/'");
+        system("cp -a '{$old}/plugins/" . dirname($plugin_file) . "/.' '{$new}/wp-content/plugins/" . dirname($plugin_file)  . "'");
 
         $this->automatic_fixes[] = "Copied plugin directory " . dirname($plugin_file) . " into the project";
       }
       else {
-        system("cp -a '{$old}/plugins/{$plugin_file}/.' '{$new}/wp-content/plugins/'");
+        system("cp '{$old}/plugins/{$plugin_file}' '{$new}/wp-content/plugins/'");
 
         $this->automatic_fixes[] = "Copied plugin file {$plugin_file} into the project";
       }
@@ -202,13 +202,13 @@ class MigrationGenerator {
     // Copy over any themes that are left over
     echo "Copying project themes\n";
     foreach($themes as $theme_dir => $theme_data) {
-      $new_theme_dir = "{$new}/wp-content/themes/" . dirname($theme_dir);
+      $new_theme_dir = "{$new}/wp-content/themes/";
 
       if(!file_exists($new_theme_dir)) {
         system("mkdir -p $new_theme_dir"); // For themes within subdirs
       }
 
-      system("cp -a '{$old}/themes/{$theme_dir}/.' '{$new_theme_dir}'");
+      system("cp -a '{$old}/themes/{$theme_dir}/.' '{$new_theme_dir}/{$theme_dir}'");
 
       $this->automatic_fixes[] = "Copied theme directory {$theme_dir} into the project";
 
