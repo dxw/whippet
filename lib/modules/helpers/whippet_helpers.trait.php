e83d8d4650e1c6e157c6dd20af2232480db2e6d5
Harry Metcalfe <harry@dxw.com>
2014-07-16 18:19:01 +0100

    find_file finds directories too

--- lib/modules/helpers/whippet_helpers.trait.php
+++ lib/modules/helpers/whippet_helpers.trait.php
@@ -67,16 +67,15 @@ trait whippet_helpers {
 
     do {
       $file_path = $path . '/' . $file;
-      if(file_exists($file_path) && is_file($file_path)) {
+
+      if(file_exists($file_path)) {
         return $file_path;
       }
+
       $path = dirname($path);
     }
-    // FIXME: Should this stop earlier?
-    //   Bundler doesn't, but has a more robust ending condition:
-    //     until !File.directory?(current) || current == previous
-    //   https://github.com/bundler/bundler/blob/d61b1ac60227b82f451c0858f60558ecbc80ee54/lib/bundler/shared_helpers.rb
-    while($path !== '/');
+    // dirname returns . if you call it on /
+    while($path !== '.');
 
     return false;
   }
@@ -89,6 +88,7 @@ trait whippet_helpers {
       "wp-content/plugins/",
       ".gitignore",
     );
+
     $missing = array();
     foreach ($whippet_files as $file) {
       if(!file_exists("{$project_dir}/{$file}")) {
