63ac618ba3ee9ca0890dbf9064562bca8181cede
Duncan Stuart <dgmstuart@gmail.com>
2014-08-13 17:41:03 +0100

    If a gitignore doesn't end with a newline, one is now added before new ignores are added - fix for github issue #3: https://github.com/dxw/whippet/issues/3

--- lib/git/gitignore.class.php
+++ lib/git/gitignore.class.php
@@ -6,10 +6,21 @@ class Gitignore {
   }
 
   function get_ignores() {
-    return file($this->ignore_file);
+    return $this->ensure_closing_newline(file($this->ignore_file));
   }
 
   function save_ignores($ignores) {
     return file_put_contents($this->ignore_file, $ignores);
+  }
+
+  private function ensure_closing_newline($ignores) {
+    $index_of_last_line = count( $ignores ) - 1;
+    $last_line = $ignores[$index_of_last_line];
+    $last_character = substr($last_line, -1);
+
+    if ($last_character != "\n") {
+      $ignores[$index_of_last_line] =  $last_line . "\n";
+    }
+    return $ignores;
   }
 }
