dc2a7295b6c7459868d9d4920668d2077be97894
Tom Adams <tom@dxw.com>
2015-03-19 09:31:16 -0400

    Allow disabling password nerfing

--- docker-images/whippet-server-custom/whippet-wp-config.php
+++ docker-images/whippet-server-custom/whippet-wp-config.php
@@ -1,17 +1,6 @@
 <?php
-// mu-plugins
-define('WPMU_PLUGIN_DIR', '/usr/src/mu-plugins');
 
-// Password nerfing
-function wp_check_password(){return true;}
-
-// Database
-define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR').':'.getenv('MYSQL_PORT_3306_TCP_PORT'));
-define('DB_NAME', getenv('MYSQL_ENV_MYSQL_DATABASE'));
-define('DB_USER', 'root');
-define('DB_PASSWORD', getenv('MYSQL_ENV_MYSQL_ROOT_PASSWORD'));
-
-// Per-project configuration goes in a separate file
+// Per-project configuration
 if (file_exists('/usr/src/app/config/server.php')) {
   require('/usr/src/app/config/server.php');
 }
@@ -20,3 +9,17 @@ if (file_exists('/usr/src/app/config/server.php')) {
 if (file_exists('/usr/src/app/config/server-local.php')) {
   require('/usr/src/app/config/server-local.php');
 }
+
+// Password nerfing
+if (!defined('DISABLE_PASSWORD_NERFING')) {
+  function wp_check_password(){return true;}
+}
+
+// mu-plugins
+define('WPMU_PLUGIN_DIR', '/usr/src/mu-plugins');
+
+// Database
+define('DB_HOST', getenv('MYSQL_PORT_3306_TCP_ADDR').':'.getenv('MYSQL_PORT_3306_TCP_PORT'));
+define('DB_NAME', getenv('MYSQL_ENV_MYSQL_DATABASE'));
+define('DB_USER', 'root');
+define('DB_PASSWORD', getenv('MYSQL_ENV_MYSQL_ROOT_PASSWORD'));
