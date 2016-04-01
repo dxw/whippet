7a5dfd9dccd472dc3ea0a03c373b06fb0e53f2c3
Tom Adams <tom@dxw.com>
2016-04-01 09:17:47 -0400

    Pre-DRY tests

--- tests/modules/helpers/dependencies_test.php
+++ tests/modules/helpers/dependencies_test.php
@@ -2,18 +2,66 @@
 
 class Modules_Helpers_Dependencies_Test extends PHPUnit_Framework_TestCase
 {
-    public function testInstall()
+    private function getWhippetLock($dependencyType, $return)
     {
-        $root = \org\bovigo\vfs\vfsStream::setup();
-        $dir = $root->url();
-
         $whippetLock = $this->getMockBuilder('\\Dxw\\Whippet\\Modules\\Helpers\\WhippetLock')
         ->disableOriginalConstructor()
         ->getMock();
 
         $whippetLock->method('getDependencies')
-        ->with('themes')
-        ->willReturn([
+        ->with($dependencyType)
+        ->willReturn($return);
+
+        return $whippetLock;
+    }
+
+    private function getFileLocator($return)
+    {
+        $fileLocator = $this->getMockBuilder('\\Dxw\\Whippet\\Modules\\Helpers\\FileLocator')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $fileLocator->method('getDirectory')
+        ->willReturn($return);
+
+        return $fileLocator;
+    }
+
+    private function getGit($cloneRepo, $checkout)
+    {
+        $git = $this->getMockBuilder('\\Dxw\\Whippet\\Git\\Git')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $git->expects($this->exactly(1))
+        ->method('clone_repo')
+        ->with($cloneRepo);
+
+        $git->expects($this->exactly(1))
+        ->method('checkout')
+        ->with($checkout);
+
+        return $git;
+    }
+
+    private function getFactory($valueMap)
+    {
+        $factory = $this->getMockBuilder('\\Dxw\\Whippet\\Factory')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $factory->method('newInstance')
+        ->will($this->returnValueMap($valueMap));
+
+        return $factory;
+    }
+
+    public function testInstall()
+    {
+        $root = \org\bovigo\vfs\vfsStream::setup();
+        $dir = $root->url();
+
+        $whippetLock = $this->getWhippetLock('themes', [
             [
                 'name' => 'my-theme',
                 'src' => 'git@git.dxw.net:wordpress-themes/my-theme',
@@ -21,34 +69,14 @@ class Modules_Helpers_Dependencies_Test extends PHPUnit_Framework_TestCase
             ],
         ]);
 
-        $git = $this->getMockBuilder('\\Dxw\\Whippet\\Git\\Git')
-        ->disableOriginalConstructor()
-        ->getMock();
+        $fileLocator = $this->getFileLocator(\Result\Result::ok($dir));
 
-        $git->expects($this->exactly(1))
-        ->method('clone_repo')
-        ->with('git@git.dxw.net:wordpress-themes/my-theme');
+        $git = $this->getGit('git@git.dxw.net:wordpress-themes/my-theme', '27ba906');
 
-        $git->expects($this->exactly(1))
-        ->method('checkout')
-        ->with('27ba906');
-
-        $factory = $this->getMockBuilder('\\Dxw\\Whippet\\Factory')
-        ->disableOriginalConstructor()
-        ->getMock();
-
-        $factory->method('newInstance')
-        ->will($this->returnValueMap([
+        $factory = $this->getFactory([
             ['\\Dxw\\Whippet\\Modules\\Helpers\\WhippetLock', $dir.'/whippet.lock', $whippetLock],
             ['\\Dxw\\Whippet\\Git\\Git', $dir.'/wp-content/themes/my-theme', $git],
-        ]));
-
-        $fileLocator = $this->getMockBuilder('\\Dxw\\Whippet\\Modules\\Helpers\\FileLocator')
-        ->disableOriginalConstructor()
-        ->getMock();
-
-        $fileLocator->method('getDirectory')
-        ->willReturn(\Result\Result::ok($dir));
+        ]);
 
         $dependencies = new \Dxw\Whippet\Modules\Helpers\Dependencies($factory, $fileLocator);
 
