ee0858fd1383de5f89300b79c72e4cd5663043bf
Tom Adams <tom@dxw.com>
2016-04-01 12:09:20 -0400

    Add update command

--- src/Modules/Dependencies.php
+++ src/Modules/Dependencies.php
@@ -4,22 +4,41 @@ namespace Dxw\Whippet\Modules;
 
 class Dependencies extends \RubbishThorClone
 {
-    public function commands()
+    public function __construct()
     {
-        $this->command('install', 'Installs dependencies (themes)');
+        parent::__construct();
+
+        $this->factory = new \Dxw\Whippet\Factory();
+        $this->fileLocator = new \Dxw\Whippet\FileLocator(getcwd());
     }
 
-    public function install()
+    public function commands()
     {
-        $factory = new \Dxw\Whippet\Factory();
-        $fileLocator = new \Dxw\Whippet\FileLocator(getcwd());
-        $dependencies = new \Dxw\Whippet\DependenciesInstaller($factory, $fileLocator);
+        $this->command('install', 'Installs dependencies');
+        $this->command('update', 'Updates dependencies to their latest versions');
+    }
 
-        $result = $dependencies->install();
-
+    private function exitIfError(\Result\Result $result)
+    {
         if ($result->isErr()) {
             echo sprintf("ERROR: %s\n", $result->getErr());
             exit(1);
         }
+    }
+
+    public function install()
+    {
+        $installer = new \Dxw\Whippet\DependenciesInstaller($this->factory, $this->fileLocator);
+
+        $this->exitIfError($installer->install());
+    }
+
+    public function update()
+    {
+        $updater = new \Dxw\Whippet\DependenciesUpdater($this->factory, $this->fileLocator);
+        $installer = new \Dxw\Whippet\DependenciesInstaller($this->factory, $this->fileLocator);
+
+        $this->exitIfError($updater->update());
+        $this->exitIfError($installer->install());
     }
 }
