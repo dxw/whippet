65a6d8a7832f9e3dc05d8262827c5a5c37bc79b3
Tom Adams <tom@dxw.com>
2016-04-11 14:40:04 -0400

    fixup! First test for Deployment

--- src/Modules/Helpers/WhippetHelpers.php
+++ src/Modules/Helpers/WhippetHelpers.php
@@ -160,8 +160,6 @@ trait WhippetHelpers
         }
 
         if (count($missing) > 0) {
-            echo "hello\n";
-            echo $project_dir;
             echo "The following files and directories are required but could not be found:\n";
             foreach ($missing as $file) {
                 echo "  {$file}\n";
