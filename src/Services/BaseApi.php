224b70cd6ee097be5a08698b8f8f01731b3c8719
Duncan Stuart <dgmstuart@gmail.com>
2016-11-30 21:46:47 +0000

    (feature) Real implementation of BaseApi
    Since we're only doing GET requests, maybe this is sufficient for now?
    
    Note there are no tests: the purpose of this object is to provide an
    interface to methods which talk over http so that we can mock them out
    in tests, and seamlessly change the implementation without needing to
    touch any other code.
    
    Note that we _can't_ easily unit test this, because of the call to
    `file_get_contents`, which is a core PHP function.
    
    Hopefully it's trivial enough that we can easily imagine all possible
    paths through it, so tests wouldn't add any value, even if we were able
    to mock out that method.
    
    If this class becomes any less trivial in future, then we should test
    it, and extract another trivial base-level object.

--- src/Services/BaseApi.php
+++ src/Services/BaseApi.php
@@ -7,5 +7,12 @@ class BaseApi
 {
     public function get($url)
     {
+        $response = file_get_contents($url);
+
+        if ($response === false) {
+            return \Result\Result::err('Failed to receive data from '.$url);
+        }
+
+        return \Result\Result::ok($response);
     }
 }
