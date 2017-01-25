1f89c6fc22ff5a9f3c292aefaddf4c4c4e02d64a
Duncan Stuart <dgmstuart@gmail.com>
2017-01-25 13:27:36 +0000

    (refactor) Use Guzzle instead of file_get_contents
    file_get_contents returns either the response body as a string, or
    false, and doesn't provide any way to get information about an error
    response.
    
    Set a user agent header so that Ops know where the requests are coming
    from.
    
    Guzzle is an OO approach to making http requests and will allow us to
    inspect responses in more detail.
    
    cURL was another approach considered, but it's a bit more bare metal and
    doesn't behave like an object (e.g. you have to open a session and close
    it when you're done)

--- src/Services/BaseApi.php
+++ src/Services/BaseApi.php
@@ -5,14 +5,23 @@ namespace Dxw\Whippet\Services;
 // A thin wrapper around the low-level API functions
 class BaseApi
 {
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client([
+            'headers' => [ 'User-Agent' => 'Whippet https://github.com/dxw/whippet/' ]
+        ]);
+    }
+
     public function get($url)
     {
-        $response = file_get_contents($url);
-
-        if ($response === false) {
+        try {
+            $response = $this->client->get($url);
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
             return \Result\Result::err('Failed to receive data from '.$url);
         }
 
-        return \Result\Result::ok($response);
+        return \Result\Result::ok($response->getBody());
     }
 }
