accca323d1a3e014af564e0e65f8d39569a13779
Duncan Stuart <dgmstuart@gmail.com>
2017-03-08 13:21:12 +0000

    (fix) Make test file PSR-2 compliant
    In a namespace, and in camel caps

--- tests/files/whippet_json_test.php
+++ tests/files/whippet_json_test.php
@@ -1,6 +1,8 @@
 <?php
 
-class Files_WhippetJson_Test extends PHPUnit_Framework_TestCase
+namespace Dxw\Whippet;
+
+class FilesWhippetJsonTest extends \PHPUnit_Framework_TestCase
 {
     use \Helpers;
 
