efab80fad4990aa4252fe88cab009bd939d3145e
Tom Adams <tom@dxw.com>
2017-06-23 15:26:41 -0400

    Indenting

--- src/Modules/Server.php
+++ src/Modules/Server.php
@@ -6,8 +6,8 @@ class Server extends \RubbishThorClone
 {
     use Helpers\ManifestIo;
     use Helpers\WhippetHelpers {
-    whippet_init as _whippet_init;
-  }
+        whippet_init as _whippet_init;
+    }
 
     public function commands()
     {
@@ -34,124 +34,124 @@ class Server extends \RubbishThorClone
         exec('docker rm whippet_mailcatcher whippet_mysql whippet_wordpress whippet_beanstalk 2>/dev/null');
     }
 
-  /*
-   * Commands
-   */
+    /*
+    * Commands
+    */
 
-  /*
-   * TODO: document
-   */
-  public function run()
-  {
-      $this->start();
-      $this->logs('wordpress');
-  }
-
-  /*
-   * TODO: document
-   */
-  public function start($argv = null)
-  {
-      // workaround for RubbishThorClone bug
-    if ($argv !== null) {
-        parent::start($argv);
-
-        return;
+    /*
+    * TODO: document
+    */
+    public function run()
+    {
+        $this->start();
+        $this->logs('wordpress');
     }
 
-      $this->whippet_init();
+    /*
+    * TODO: document
+    */
+    public function start($argv = null)
+    {
+        // workaround for RubbishThorClone bug
+        if ($argv !== null) {
+            parent::start($argv);
 
-    # Ensure data container exists
-    exec('docker run --label=com.dxw.whippet=true --label=com.dxw.data=true --name='.escapeshellarg($this->mysql_data).' -v /var/lib/mysql mysql /bin/true 2>/dev/null');
+            return;
+        }
 
-    # Stop/delete existing containers
-    echo "Stopping already-running containers\n";
-      $this->_stop();
+        $this->whippet_init();
 
-      $output = null;
-      $return = null;
+        # Ensure data container exists
+        exec('docker run --label=com.dxw.whippet=true --label=com.dxw.data=true --name='.escapeshellarg($this->mysql_data).' -v /var/lib/mysql mysql /bin/true 2>/dev/null');
 
-    # Start other containers
-    exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_mailcatcher -p 1080:1080 schickling/mailcatcher 2>/dev/null', $output, $return);
-      if ($return !== 0) {
-          echo "Mailcatcher container failed to start\n";
-          exit(1);
-      }
-      exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_beanstalk schickling/beanstalkd 2>/dev/null', $output, $return);
-      if ($return !== 0) {
-          echo "Beanstalk container failed to start\n";
-          exit(1);
-      }
-      exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_mysql --volumes-from='.escapeshellarg($this->mysql_data).' -e MYSQL_DATABASE=wordpress -e MYSQL_ROOT_PASSWORD=foobar mysql 2>/dev/null', $output, $return);
-      if ($return !== 0) {
-          echo "MySQL container failed to start\n";
-          exit(1);
-      }
-      exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_wordpress -v '.escapeshellarg($this->project_dir).':/usr/src/app -v '.escapeshellarg($this->project_dir).'/wp-content:/var/www/html/wp-content -p 80:80 --link=whippet_mysql:mysql --link=whippet_mailcatcher:mailcatcher --link=whippet_beanstalk:beanstalk -e PROJECT_ID='.escapeshellarg(md5($this->project_dir)).' thedxw/whippet-wordpress 2>/dev/null', $output, $return);
-      if ($return !== 0) {
-          echo "WordPress container failed to start\n";
-          exit(1);
-      }
+        # Stop/delete existing containers
+        echo "Stopping already-running containers\n";
+        $this->_stop();
 
-      echo "Started whippet containers\n";
-  }
+        $output = null;
+        $return = null;
 
-  /*
-   * TODO: document
-   */
-  public function stop()
-  {
-      $this->whippet_init();
+        # Start other containers
+        exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_mailcatcher -p 1080:1080 schickling/mailcatcher 2>/dev/null', $output, $return);
+        if ($return !== 0) {
+            echo "Mailcatcher container failed to start\n";
+            exit(1);
+        }
+        exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_beanstalk schickling/beanstalkd 2>/dev/null', $output, $return);
+        if ($return !== 0) {
+            echo "Beanstalk container failed to start\n";
+            exit(1);
+        }
+        exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_mysql --volumes-from='.escapeshellarg($this->mysql_data).' -e MYSQL_DATABASE=wordpress -e MYSQL_ROOT_PASSWORD=foobar mysql 2>/dev/null', $output, $return);
+        if ($return !== 0) {
+            echo "MySQL container failed to start\n";
+            exit(1);
+        }
+        exec('docker run -d --label=com.dxw.whippet=true --label=com.dxw.data=false --name=whippet_wordpress -v '.escapeshellarg($this->project_dir).':/usr/src/app -v '.escapeshellarg($this->project_dir).'/wp-content:/var/www/html/wp-content -p 80:80 --link=whippet_mysql:mysql --link=whippet_mailcatcher:mailcatcher --link=whippet_beanstalk:beanstalk -e PROJECT_ID='.escapeshellarg(md5($this->project_dir)).' thedxw/whippet-wordpress 2>/dev/null', $output, $return);
+        if ($return !== 0) {
+            echo "WordPress container failed to start\n";
+            exit(1);
+        }
 
-      $this->_stop();
+        echo "Started whippet containers\n";
+    }
 
-      echo "Stopped and removed all whippet non-data containers\n";
-  }
+    /*
+    * TODO: document
+    */
+    public function stop()
+    {
+        $this->whippet_init();
 
-  /*
-   * TODO: document
-   */
-  public function db($command = null)
-  {
-      $this->whippet_init();
+        $this->_stop();
 
-      if ($command === 'connect' || $command === null) {
-          passthru('docker run --label=com.dxw.whippet=true --label=com.dxw.data=false -ti --rm --link=whippet_mysql:mysql mysql sh -c \'MYSQL_PWD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD" exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot "$MYSQL_ENV_MYSQL_DATABASE"\'');
-      } elseif ($command === 'dump') {
-          passthru('docker run --label=com.dxw.whippet=true --label=com.dxw.data=false -ti --rm --link=whippet_mysql:mysql mysql sh -c \'MYSQL_PWD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD" exec mysqldump -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot "$MYSQL_ENV_MYSQL_DATABASE"\'');
-      } elseif ($command === 'undump') {
-          passthru('docker run --label=com.dxw.whippet=true --label=com.dxw.data=false -i --rm --link=whippet_mysql:mysql mysql sh -c \'MYSQL_PWD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD" exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot "$MYSQL_ENV_MYSQL_DATABASE"\'');
-      }
-  }
+        echo "Stopped and removed all whippet non-data containers\n";
+    }
 
-  /*
-   * TODO: document
-   */
-  public function ps()
-  {
-      $this->whippet_init();
+    /*
+    * TODO: document
+    */
+    public function db($command = null)
+    {
+        $this->whippet_init();
 
-      $regexp = '(^CONTAINER ID|whippet_wordpress\s*$|whippet_mysql\s*$|whippet_mailcatcher\s*$|'.$this->mysql_data.'\s*$)';
-      passthru('docker ps -a | grep -E '.escapeshellarg($regexp));
-  }
+        if ($command === 'connect' || $command === null) {
+            passthru('docker run --label=com.dxw.whippet=true --label=com.dxw.data=false -ti --rm --link=whippet_mysql:mysql mysql sh -c \'MYSQL_PWD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD" exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot "$MYSQL_ENV_MYSQL_DATABASE"\'');
+        } elseif ($command === 'dump') {
+            passthru('docker run --label=com.dxw.whippet=true --label=com.dxw.data=false -ti --rm --link=whippet_mysql:mysql mysql sh -c \'MYSQL_PWD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD" exec mysqldump -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot "$MYSQL_ENV_MYSQL_DATABASE"\'');
+        } elseif ($command === 'undump') {
+            passthru('docker run --label=com.dxw.whippet=true --label=com.dxw.data=false -i --rm --link=whippet_mysql:mysql mysql sh -c \'MYSQL_PWD="$MYSQL_ENV_MYSQL_ROOT_PASSWORD" exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot "$MYSQL_ENV_MYSQL_DATABASE"\'');
+        }
+    }
 
-  /*
-   * TODO: document
-   */
-  public function logs($container)
-  {
-      $this->whippet_init();
+    /*
+    * TODO: document
+    */
+    public function ps()
+    {
+        $this->whippet_init();
 
-      passthru('docker logs -f whippet_'.escapeshellarg($container));
-  }
+        $regexp = '(^CONTAINER ID|whippet_wordpress\s*$|whippet_mysql\s*$|whippet_mailcatcher\s*$|'.$this->mysql_data.'\s*$)';
+        passthru('docker ps -a | grep -E '.escapeshellarg($regexp));
+    }
 
-  /*
-   * TODO: document
-   */
-  public function console()
-  {
-      $this->whippet_init();
+    /*
+    * TODO: document
+    */
+    public function logs($container)
+    {
+        $this->whippet_init();
 
-      passthru('docker exec -ti whippet_wordpress bash');
-  }
+        passthru('docker logs -f whippet_'.escapeshellarg($container));
+    }
+
+    /*
+    * TODO: document
+    */
+    public function console()
+    {
+        $this->whippet_init();
+
+        passthru('docker exec -ti whippet_wordpress bash');
+    }
 };
