7f45845b5235146f7d195aa3ffdf4747b0c3190b
Rob Skilling <rob@dxw.com>
2021-09-29 17:08:55 +0100

    Add validator class
    This class will be used in a `whippet deps validate` command, in a
    forthcoming commit, to validate whippet.json and whippet.lock files. It
    checks:
    
    1. That both are present and valid JSON
    2. That the hash in whippet.lock is as expected based on the content of
    whippet.json
    3. That there are a matching number of dependencies in each file
    4. That each dependency listed in whippet.json has a corresponding entry
    in whippet.lock
    5. That all entries in whippet.lock are correctly formed (i.e. have a
    name, revision, and src property).

---
+++ src/Dependencies/Validator.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Dxw\Whippet\Dependencies;
+
+class Validator
+{
+    public function __construct(
+        \Dxw\Whippet\Factory $factory,
+        \Dxw\Whippet\ProjectDirectory $dir
+    ) {
+        $this->factory = $factory;
+        $this->dir = $dir;
+    }
+
+    public function validate()
+    {
+        $whippetLock = $this->loadWhippetLock();
+        if ($whippetLock->isErr()) {
+            return \Result\Result::err(sprintf(
+                'whippet.lock error: %s',
+                $whippetLock->getErr()
+            ));
+        } else {
+            $whippetLock = $whippetLock->unwrap();
+        }
+
+        $whippetJson = $this->loadWhippetJson();
+        if ($whippetJson->isErr()) {
+            return \Result\Result::err(sprintf(
+                'whippet.json error: %s',
+                $whippetJson->getErr()
+            ));
+        } else {
+            $whippetJson = $whippetJson->unwrap();
+        }
+
+        // Check hashes
+        if ($this->whippetJsonHash() !== $whippetLock->getHash()) {
+            return \Result\Result::err(
+                'hash mismatch between whippet.json and whippet.lock'
+            );
+        };
+
+        // Check that entries in whippet.json
+        // match entries in whippet.lock
+        foreach (['themes', 'plugins'] as $type) {
+            $whippetJsonDependencies = $whippetJson->getDependencies($type);
+            $whippetLockDependencies = $whippetLock->getDependencies($type);
+            if (count($whippetJsonDependencies) !== count($whippetLockDependencies)) {
+                return \Result\Result::err(sprintf('Mismatched dependencies count for type %s', $type));
+            }
+
+            foreach ($whippetJsonDependencies as $whippetJsonDependency) {
+                $matchFound = false;
+                foreach ($whippetLockDependencies as $whippetLockDependency) {
+                    if ($whippetJsonDependency["name"] == $whippetLockDependency["name"]) {
+                        $matchFound = true;
+                    }
+                }
+                if (!$matchFound) {
+                    return \Result\Result::err(sprintf('No entry found in whippet.lock for %s: %s', $type, $whippetJsonDependency["name"]));
+                }
+            }
+
+            foreach ($whippetLockDependencies as $whippetLockDependency) {
+                foreach (['revision', 'src'] as $property) {
+                    if (!array_key_exists($property, $whippetLockDependency)) {
+                        return \Result\Result::err(sprintf("Missing %s property in whippet.lock for %s: %s", $property, $type, $whippetLockDependency["name"]));
+                    }
+                }
+            }
+        }
+
+        echo "Valid whippet.json and whippet.lock \n";
+
+        return \Result\Result::ok();
+    }
+
+    private function loadWhippetLock()
+    {
+        return $this->factory->callStatic('\\Dxw\\Whippet\\Files\\WhippetLock', 'fromFile', $this->dir.'/whippet.lock');
+    }
+
+    private function loadWhippetJson()
+    {
+        return $this->factory->callStatic('\\Dxw\\Whippet\\Files\\WhippetJson', 'fromFile', $this->dir.'/whippet.json');
+    }
+
+    private function whippetJsonHash()
+    {
+        $contents = file_get_contents($this->dir.'/whippet.json');
+
+        // Strip CR for git/Windows compatibility
+        $contents = strtr($contents, ["\r" => '']);
+
+        return sha1($contents);
+    }
+}
